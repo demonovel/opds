41ddb041d54d6cdde8cb2d7a9a201c48c1a5459b
bluelovers <codelovers@users.sourceforge.net>
2020-02-23 15:27:11 +0800

    .

--- packages/build-json-cache/lib/all/json-to-ts.ts
+++ packages/build-json-cache/lib/all/json-to-ts.ts
@@ -7,13 +7,13 @@ import { join } from "path";
 import { __root } from '../../__root';
 
 readJSON(join(__root, '.cache', `./build.all.array.json`))
-	.then(data => outputFile(join(__root, '.cache', `./build.all.array.ts`), `export default ${JSON.stringify(data)}`))
+	.then(data => outputFile(join(__root, '.cache', `./build.all.array.js`), `module.exports = ${JSON.stringify(data)}`))
 ;
 
 readJSON(join(__root, '.cache', `./build.all.json`))
-	.then(data => outputFile(join(__root, '.cache', `./build.all.ts`), `export default ${JSON.stringify(data)}`))
+	.then(data => outputFile(join(__root, '.cache', `./build.all.js`), `module.exports = ${JSON.stringify(data)}`))
 ;
 
 readJSON(join(__root, '.cache', 'temp', `./titles.json`))
-	.then(data => outputFile(join(__root, '.cache', 'temp', `./titles.ts`), `export default ${JSON.stringify(data)}`))
+	.then(data => outputFile(join(__root, '.cache', 'temp', `./titles.js`), `module.exports = ${JSON.stringify(data)}`))
 ;
